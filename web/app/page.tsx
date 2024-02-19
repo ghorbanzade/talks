9f82266584c9cebb244f0f8382a4bc87c05adb47
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2024-02-18 23:08:37 -0800

    update web page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,53 +1,99 @@
 // Copyright 2024 Pejman Ghorbanzade
 
-import Image from 'next/image';
-import Markdown from 'markdown-to-jsx';
+import { format } from 'date-fns';
 
-import profilePicture from '@/public/images/pejman.jpg';
-import SocialLinks from '@/components/social';
 import DarkModeButton from '@/components/darkMode';
 
-export default async function Home() {
-  const aboutPage = await fetch(
-    'https://raw.githubusercontent.com/ghorbanzade/ghorbanzade/main/Readme.md'
+type Talk = Partial<{
+  date: string;
+  location: string;
+  conference: string;
+  duration: number;
+  tags: string[];
+  title: string;
+  links: Partial<
+    Record<
+      | 'registration'
+      | 'pdf'
+      | 'html'
+      | 'youtube'
+      | 'cover'
+      | 'coverImage'
+      | 'repository',
+      string
+    >
+  >;
+}>;
+
+async function getTalks(): Promise<Talk[]> {
+  const content = await fetch(
+    'https://raw.githubusercontent.com/ghorbanzade/talks/main/web/talks.json'
   );
-  const fileContent = await aboutPage.text();
+  const raw = JSON.parse(await content.text()) as Talk[];
+  const highlighted = raw.filter((v) => v.tags?.includes('highlighted'));
+  return highlighted;
+}
+
+function ListView({ talks }: { talks: Talk[] }) {
   return (
-    <main className="flex h-screen md:items-center">
-      <div className="mx-auto max-w-screen-lg md:grid md:grid-cols-4 md:gap-3">
-        <div className="rounded-lg border-slate-300 bg-white p-6 dark:border-slate-700 dark:bg-gradient-to-bl dark:from-black dark:to-slate-900 md:col-span-1 md:border">
-          <div className="space-y-4 md:flex md:h-full md:flex-wrap md:content-between">
-            <div className="hidden py-2 text-center md:block">
-              <Image
-                className="rounded-lg"
-                src={profilePicture}
-                alt="Pejman Ghorbanzade"
-              />
+    <div className="rounded-lg border-slate-300 bg-white px-6 dark:border-slate-700 dark:bg-gradient-to-br dark:from-black dark:to-slate-900 border divide-y divide-slate-800">
+      {talks.map((talk, index) => (
+        <div
+          key={index}
+          className="min-h-24 prose prose-base prose-slate min-w-full dark:prose-invert py-6"
+        >
+          <div className="flex justify-between">
+            <div>
+              <div className="">
+                {talk.conference}, {talk.location}
+              </div>
+              <div className="text-xl">{talk.title}</div>
             </div>
-            <div className="w-full text-center">
-              <h1 className="text-lg font-semibold text-slate-900 dark:text-slate-200">
-                Pejman Ghorbanzade
-              </h1>
-              <h2 className="text-slate-600 dark:text-slate-300">
-                Software Engineer
-              </h2>
+            <div className="flex items-center">
+              <time dateTime={talk.date}>
+                {format(new Date(talk.date!), 'LLL yyyy')}
+              </time>
             </div>
-            <SocialLinks />
+          </div>
+          <div className="flex space-x-6">
+            {talk.links?.registration && (
+              <a href={talk.links.registration}>Registration</a>
+            )}
+            {talk.links?.youtube && <a href={talk.links.youtube}>Recording</a>}
+            {talk.links?.pdf && talk.links?.html && (
+              <div>
+                Slides (<a href={talk.links.html}>HTML</a>,{' '}
+                <a href={talk.links.pdf}>PDF</a>)
+              </div>
+            )}
+            {talk.links?.repository && (
+              <a href={talk.links.repository}>Repository</a>
+            )}
           </div>
         </div>
-        <div className="relative rounded-lg border-slate-300 bg-white p-6 dark:border-slate-700 dark:bg-gradient-to-br dark:from-black dark:to-slate-900 md:col-span-3 md:border">
+      ))}
+    </div>
+  );
+}
+
+export default async function Home() {
+  const talks = await getTalks();
+  return (
+    <main>
+      <div className="mx-auto max-w-screen-lg space-y-4 py-4">
+        <div className="relative min-h-24 rounded-lg border-slate-300 bg-white p-6 dark:border-slate-700 dark:bg-gradient-to-br dark:from-black dark:to-slate-900 border">
+          <div className="prose dark:prose-invert">
+            <div className="text-2xl">Public Talks</div>
+            <div>Pejman Ghorbanzade</div>
+          </div>
           <div className="absolute right-8 top-8">
             <DarkModeButton />
           </div>
-          <Markdown className="wsl-mark prose prose-base prose-slate min-w-full dark:prose-invert">
-            {fileContent}
-          </Markdown>
+        </div>
+        <div>
+          <ListView talks={talks} />
         </div>
       </div>
     </main>
-    // <main className="h-screen py-[10vh] w-4/5 m-auto grid grid-cols-3 grid-rows-3 gap-4">
-    //   <div className="border border-slate-200">CppCon23</div>
-    //   <div className="border border-slate-200">CppCon21</div>
-    // </main>
   );
 }
