f330402c454cd4456b455a9ab9f3e4951565cde6
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2024-02-19 10:18:50 -0800

    update webpage

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,37 +1,58 @@
 // Copyright 2024 Pejman Ghorbanzade
 
 import { format } from 'date-fns';
-
+import { FiStar } from 'react-icons/fi';
 import DarkModeButton from '@/components/darkMode';
 
-type Talk = Partial<{
+type Links = Partial<
+  Record<
+    | 'registration'
+    | 'pdf'
+    | 'html'
+    | 'youtube'
+    | 'cover'
+    | 'coverImage'
+    | 'repository',
+    string
+  >
+>;
+
+type Talk = {
   date: string;
-  location: string;
+  location?: string;
   conference: string;
   duration: number;
   tags: string[];
   title: string;
-  links: Partial<
-    Record<
-      | 'registration'
-      | 'pdf'
-      | 'html'
-      | 'youtube'
-      | 'cover'
-      | 'coverImage'
-      | 'repository',
-      string
-    >
-  >;
-}>;
+  links: Links;
+};
 
 async function getTalks(): Promise<Talk[]> {
   const content = await fetch(
     'https://raw.githubusercontent.com/ghorbanzade/talks/main/web/talks.json'
   );
   const raw = JSON.parse(await content.text()) as Talk[];
-  const highlighted = raw.filter((v) => v.tags?.includes('highlighted'));
-  return highlighted;
+  return raw.sort((a, b) => {
+    const aScore = a.tags.includes('highlighted');
+    const bScore = b.tags.includes('highlighted');
+    return aScore !== bScore
+      ? Number(bScore) - Number(aScore)
+      : +new Date(b.date!) - +new Date(a.date!);
+  });
+}
+
+function SlidesLink({ links }: { links: Links }) {
+  return links.pdf || links.html ? (
+    <div>
+      <span>Slides (</span>
+      {links.html && <a href={links.html}>HTML</a>}
+      {links.html && links.pdf && <span>, </span>}
+      {links.pdf && <a href={links.pdf}>PDF</a>}
+      <span>)</span>
+    </div>
+  ) : (
+    <></>
+  );
 }
 
 function ListView({ talks }: { talks: Talk[] }) {
@@ -44,31 +65,38 @@ function ListView({ talks }: { talks: Talk[] }) {
         >
           <div className="flex justify-between">
             <div>
-              <div className="">
-                {talk.conference}, {talk.location}
+              <div className="flex items-center space-x-2">
+                <div>
+                  <span className="font-semibold">{talk.conference}</span>
+                  {talk.location && (
+                    <span className="font-light">, {talk.location}</span>
+                  )}
+                </div>
+                {talk.tags.includes('highlighted') ? (
+                  <FiStar className="text-slate-300" />
+                ) : (
+                  <></>
+                )}
               </div>
               <div className="text-xl">{talk.title}</div>
+              <div className="flex space-x-6">
+                {talk.links?.registration && (
+                  <a href={talk.links.registration}>Registration</a>
+                )}
+                {talk.links?.youtube && (
+                  <a href={talk.links.youtube}>Recording</a>
+                )}
+                <SlidesLink links={talk.links} />
+                {talk.links?.repository && (
+                  <a href={talk.links.repository}>Repository</a>
+                )}
+              </div>
             </div>
             <div className="flex items-center">
               <time dateTime={talk.date}>
                 {format(new Date(talk.date!), 'LLL yyyy')}
               </time>
             </div>
-          </div>
-          <div className="flex space-x-6">
-            {talk.links?.registration && (
-              <a href={talk.links.registration}>Registration</a>
-            )}
-            {talk.links?.youtube && <a href={talk.links.youtube}>Recording</a>}
-            {talk.links?.pdf && talk.links?.html && (
-              <div>
-                Slides (<a href={talk.links.html}>HTML</a>,{' '}
-                <a href={talk.links.pdf}>PDF</a>)
-              </div>
-            )}
-            {talk.links?.repository && (
-              <a href={talk.links.repository}>Repository</a>
-            )}
           </div>
         </div>
       ))}
@@ -86,7 +114,7 @@ export default async function Home() {
             <div className="text-2xl">Public Talks</div>
             <div>Pejman Ghorbanzade</div>
           </div>
-          <div className="absolute right-8 top-8">
+          <div className="absolute right-6 top-10">
             <DarkModeButton />
           </div>
         </div>
